98e80038c60fa0bc6c3395c90e2a9b5574f8e330
Tri <vutriminh@gmail.com>
2024-09-26 05:32:57 -0400

    completed assignment 1

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -60,9 +60,10 @@ public class TechJobs {
                 // What is their search term?
                 System.out.println("\nSearch term:");
                 String searchTerm = in.nextLine();
+                searchTerm = searchTerm.toLowerCase();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not implemented yet.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -77,7 +78,7 @@ public class TechJobs {
         Boolean validChoice = false;
         String[] choiceKeys = new String[choices.size()];
 
-        // Put the choices in an ordered structure so we can
+        // Put the choices in an ordered structure, so we can
         // associate an integer with each one
         int i = 0;
         for (String choiceKey : choices.keySet()) {
@@ -120,6 +121,28 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+//        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+        } else {
+            int count = 0;
+            int size = someJobs.size();
+            for (HashMap<String, String> map : someJobs) {
+                System.out.println("*****");
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+
+                    System.out.println(key + ": " + value);
+                }
+                System.out.println("*****");
+
+                if (count < size - 1) {
+                    count++;
+                    System.out.print("\n");
+                }
+            }
+
+        }
     }
 }
